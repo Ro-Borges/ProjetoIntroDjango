d165fb14b7d8c31db4ffc3a94a89b141feee292b
Ro-Borges <rodrigo.borges.freitas@gmail.com>
2026-01-05 01:46:45 -0300

    primeira página

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'helloworld'
-    'website'
+    'helloworld',
+    'website',
 ]
 
 MIDDLEWARE = [
--- helloworld/urls.py
+++ helloworld/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls.conf import include
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    # Inclui as URLs do app website
+    path('', include('website.urls', namespace='website')),
+    # Interface administrativa
     path('admin/', admin.site.urls),
 ]
