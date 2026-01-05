6dd3513b324e9ba0f412f9b0def34e0e50c539bd
Ro-Borges <rodrigo.borges.freitas@gmail.com>
2026-01-05 02:11:37 -0300

    listview

--- website/views.py
+++ website/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+
+from helloworld.models import Funcionario
 
 def index(request):
     return render(request, "website/index.html")
+
+class FuncionarioListView(ListView):
+    template_name = "website/lista.html"
+    model = Funcionario
+    context_object_name = "funcionarios"
